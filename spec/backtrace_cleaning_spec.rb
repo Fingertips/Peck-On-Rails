fbce87640951778dcc2a450860046e283d41bed0
Manfred Stienstra <manfred@fngtps.com>
2013-06-21 23:13:31 +0200

    Add a rather shallow test for backtrace cleaner.

---
+++ spec/backtrace_cleaning_spec.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+require File.expand_path('../preamble', __FILE__)
+
+describe Peck::Rails::BacktraceCleaning do
+  DEFAULT_CLEANER_RE = /^default/
+
+  def clean_backtrace(backtrace)
+    backtrace.reject do |line|
+      line =~ DEFAULT_CLEANER_RE
+    end
+  end
+
+  include Peck::Rails::BacktraceCleaning
+
+  it "overrides the backtrace cleaner method in Peck to include Rails' backtrace cleaner" do
+    clean_backtrace([
+      "default - line 1",
+      "default - line 2",
+      "other stuff"
+    ]).should == [
+      "other stuff"
+    ]
+  end
+end
