08073cb3932db827587554abef77892c2e756780
Manfred Stienstra <manfred@fngtps.com>
2013-08-01 15:19:13 +0200

    Improve coverage for specification macros.

--- spec/specification_spec.rb
+++ spec/specification_spec.rb
@@ -3,10 +3,37 @@
 require 'preamble'
 
 describe Peck::Should::Specification do
-  it "initializes a potential specification for checking if a resource is found" do
-    spec = Peck::Should::Specification.new(self).find
-    spec.negated.should == false
-    spec.method.should == :status
-    spec.expected.should == :ok
+  before do
+    @spec = Peck::Should::Specification.new(self)
+  end
+
+  describe "initializes a potential specification for checking if a resource" do
+    it "requires a login" do
+      rec = @spec.require_login
+      rec.negated.should == false
+      rec.method.should == :login_required?
+      rec.expected.should == true
+    end
+
+    it "does not require a login" do
+      rec = @spec.not.require_login
+      rec.negated.should == true
+      rec.method.should == :login_required?
+      rec.expected.should == true
+    end
+
+    it "is found" do
+      rec = @spec.find
+      rec.negated.should == false
+      rec.method.should == :status
+      rec.expected.should == :ok
+    end
+
+    it "is not found" do
+      rec = @spec.not.find
+      rec.negated.should == true
+      rec.method.should == :status
+      rec.expected.should == :not_found
+    end
   end
 end
