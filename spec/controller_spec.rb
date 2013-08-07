14fad390d71607baa49f9f08621b3407c397b1a2
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2013-08-07 11:53:32 +0200

    Add simple tests for Specification#find

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,13 +1,16 @@
 # encoding: utf-8
 
-require 'preamble'
+require File.expand_path('../rails/rails40/preamble', __FILE__)
 
 describe AuthorsController do
   it "has the correct routes configured" do
-    @routes.should.be.kind_of(Rails::FakeRouteSet)
+    @routes.should.be.kind_of(ActionDispatch::Routing::RouteSet)
   end
 
   it "has the right controller class configured" do
     _controller_class.should == AuthorsController
   end
+
+  should.find.get :index
+  should.not.find.get :show
 end
--- spec/shared.rb
+++ spec/shared.rb
@@ -16,41 +16,33 @@ class Peck
 end
 
 require 'peck_on_rails'
-
 require 'rails'
 require 'active_record'
 require 'action_controller'
 
 puts "Running on Rails #{Rails.version}"
 
-Dir.glob(File.expand_path('../implementation/**/*.rb', __FILE__)).each do |lib|
-  require lib
-end
-
 module Rails
-  class FakeRouteSet
-    def extra_keys(params)
-      params
-    end
-  end
-
-  class FakeApplication
-    def routes
-      @routes ||= Rails::FakeRouteSet.new
-    end
-
-    def env_config
-      {}
-    end
-  end
-
   def self.root
     File.expand_path('../implementation', __FILE__)
   end
+end
 
-  def self.application
-    @application ||= Rails::FakeApplication.new
+module Vanilla
+  class Application < Rails::Application
+    config.eager_load = false
+    config.secret_key_base = 'test'
   end
+end
+
+Vanilla::Application.initialize!
+
+Vanilla::Application.routes.draw do
+  resources :authors
+end
+
+Dir.glob(File.expand_path('../implementation/**/*.rb', __FILE__)).each do |lib|
+  require lib
 end
 
 ActionController::Base.view_paths = File.join(Rails.root, 'app/views')
@@ -61,4 +53,3 @@ class FakeContext
     attributes.each { |k,v| send("#{k}=", v) }
   end
 end
-
