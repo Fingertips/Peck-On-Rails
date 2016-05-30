4b8af6cb116cccb7eecf606824264bd8c1eee301
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-05-30 18:14:06 +0200

    Add spec/rails/rails50/preamble.rb

---
+++ spec/rails/rails50/preamble.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+gem 'rails', '~> 5.0.0.rc1'
+
+require File.expand_path('../../../shared', __FILE__)
+
+Rails.application.routes.draw do
+  get 'authors' => 'authors#index'
+  get 'authors/:id' => 'authors#show'
+  get 'authors/restricted' => 'authors#restricted'
+  get 'authors/disallowed' => 'authors#disallowed'
+end
+
